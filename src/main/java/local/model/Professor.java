35bce0ca373a70f42912f1e2fd1ad8b1db0e8430
Carlos M Etchepare <categten7@gmail.com>
2018-04-25 11:45:48 -0300

    Inclusão da classe Professor

---
+++ src/main/java/local/model/Professor.java
@@ -0,0 +1,15 @@
+package local.model;
+
+public class Professor {
+	
+	private String nome;
+	
+	public String getNome() {
+        return nome;
+    }
+
+    public void setAula(String nome) {
+        this.nome = nome;
+    }
+
+}
