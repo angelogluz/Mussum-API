d56b2938e4f4f6ab06bc707c97137ef34339372d
Angelo Luz <angelogl@gmail.com>
2018-05-03 00:04:18 -0300

    Adicionada autenticação por JWT - ainda em memória

--- src/main/java/local/config/AuthorizationServerConfig.java
+++ src/main/java/local/config/AuthorizationServerConfig.java
@@ -4,17 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.BeanIds;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 
-    @Configuration
+@Configuration
     @EnableAuthorizationServer
     public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
@@ -36,12 +35,21 @@ import org.springframework.security.oauth2.provider.token.store.InMemoryTokenSto
         public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
             endpoints
                     .tokenStore(tokenStore())
+                    .accessTokenConverter(acessTokenConverter())
                     .authenticationManager(authenticationManager);
+        }
+
+
+        @Bean
+        public JwtAccessTokenConverter acessTokenConverter() {
+            JwtAccessTokenConverter access = new JwtAccessTokenConverter();
+            access.setSigningKey("senac2018");
+            return access;
         }
 
         @Bean
         public TokenStore tokenStore() {
-            return new InMemoryTokenStore();
+            return new JwtTokenStore(acessTokenConverter());
         }
 
 
